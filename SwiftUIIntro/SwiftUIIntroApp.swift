af33a0311920e6428887aace22b1b930b244548a
JC Manikis <jmanikis@icloud.com>
2021-12-27 13:51:09 -0500

    Initial Commit

---
+++ SwiftUIIntro/SwiftUIIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIntroApp.swift
+//  SwiftUIIntro
+//
+//  Created by JC Manikis on 2021-12-27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
